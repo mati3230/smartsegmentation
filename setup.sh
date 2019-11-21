30b6b5a0f7c8590b6e10994fdb5329adbf05ee39
Marcel Tiator <tiator.ma@gmail.com>
2019-11-21 14:52:32 +0100

    changed tensorflow installation

--- setup.sh
+++ setup.sh
@@ -11,9 +11,22 @@ cd stable-baselines
 pip install -e .
 cd ..
 
+
+tf_version="-gpu==1.14"
+if [ -n "$1" ]; then # if first parameter passed
+    if [ $1 = "tensorflow==1.5" ] 
+    then
+        tf_version="==1.5"
+    elif [ $1 = "tensorflow==1.14" ] 
+    then
+        tf_version="==1.14"
+    fi
+fi
+echo "tensorflow$tf_version will be installed" 
+
+pip install -r requirements.txt
+
 git clone https://github.com/mati3230/pyntcloud.git
 cd pyntcloud
 pip install -e .
 cd ..
-
-pip install -r requirements.txt
